bbe0d8655fd1090e2a0d4107975848712433d496
quochuy2405 <huy01682312920@gmail.com>
2024-03-04 14:35:05 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@nuxt/ui', 'nuxt-pdfmake'],
+    modules: ['@nuxt/ui', 'nuxt-pdfmake', '@vue-email/nuxt'],
 })
