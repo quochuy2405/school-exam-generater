64feacbc34e4a92b288d81f19ead7b2ca35e6332
quochuy2405 <huy01682312920@gmail.com>
2024-03-23 10:29:15 +0700

    update search

--- server/api/student/class.post.ts
+++ server/api/student/class.post.ts
@@ -3,7 +3,10 @@ import { Student } from '~~/server/models/student.model'
 export default defineEventHandler(async (event) => {
     const payload = await readBody(event)
     const CLASS = payload.CLASS
-    const student = await Student.find({ CLASS })
+    const AREA = payload.AREA
+    const COSO = payload.COSO
+    const KHOI = payload.KHOI
+    const student = await Student.find({ CLASS, AREA, COSO, KHOI })
     if (student.length == 0) {
         return setResponseStatus(event, 404, 'Not Found')
     }
